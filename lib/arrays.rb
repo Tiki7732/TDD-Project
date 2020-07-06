ef3c83588cb03982acd812223023dbd2883b4019
tiki7732 <lsaidah@gmail.com>
2020-07-06 13:13:29 -0500

    changing the two_sum method

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -3,3 +3,13 @@ def my_uniq(array)
     array.each {|item| array_hash[item] += 1}
     array_hash.keys
 end
+
+def two_sum(array)
+    indicies = []
+    array.each_with_index do |num1, ind1|
+        array[ind1 + 1..-1].each_with_index do |num2, ind2|
+            indicies.push([ind1, ind2+1+ind1]) if (num1 + num2 == 0)
+        end
+    end
+    indicies
+end
