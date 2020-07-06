3a9a5086a2eda43f574ba529188d6481b9690b2e
tiki7732 <lsaidah@gmail.com>
2020-07-06 12:45:58 -0500

    filling out tests for two_sum method

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -2,17 +2,17 @@ require 'arrays'
    
 describe "my_uniq" do
     let(:dups_array) {[4, 2, 3, 7, 2, 9, 9, 0, 7, 4, 1, 6]}
-    #let(:dupless_array) {my_uniq(dups_array.dup)}
-    let(:dupless_array)  {[4, 2, 3, 7, 9, 0, 1, 6]}
+    let(:dupless_array) {my_uniq(dups_array.dup)}
+    #let(:dupless_array)  {[4, 2, 3, 7, 9, 0, 1, 6]}
     it  'removes duplicates' do
-        # dups_array.each do |item|
-        #    expect(dupless_array.count(item)).to eq(1)
-        # end
-        expect(my_uniq(dups_array)).to eq(dupless_array)
+        dups_array.each do |item|
+           expect(dupless_array.count(item)).to eq(1)
+        end
+        #expect(my_uniq(dups_array)).to eq(dupless_array)
     end
 
     it "doesn't change original" do
-        expect(my_uniq(dups_array)).to_not change{dups_array}
+        expect{my_uniq(dups_array)}.to_not change {dups_array}
     end
 
     it "returns items in order they appeared" do
@@ -27,11 +27,26 @@ end
 
 describe "#two_sum" do
     let(:test) {[-1, 0, 2, -2, 1, 0, -3, 4, -4, 3, 1]}
+    let(:pairs) {two_sum(test)}
 
-    it "finds pairs whose elements add up to zero"
+    it "finds pairs whose elements add up to zero" do
+        #expect(pairs.length).to eq(6)
+        pairs.each do |pair|
+            expect(test[pair.first] + test[pair.last]).to eq(0)
+        end
+    end
 
-    it "sorts with smaller elements first"
+    it "smaller elements first" do
+        pairs.each do |pair|
+            expect(pair.first).to be < (pair.last)
+        end
+    end
 
-    it "sorts with smaller pair first"
+    it "smaller pair first" do
+        pairs[0..-2].each_with_index do |pair, ind|
+            expect(pair.sum).to be <= (pairs[ind+1].sum)
+        end
+    end
+
 end
 
