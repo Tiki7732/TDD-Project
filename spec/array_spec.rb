f390f53db7ad3599aeeb32e9af95d064edba96e3
tiki7732 <lsaidah@gmail.com>
2020-07-06 12:01:35 -0500

    add a ordered appeared test

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,6 +1,6 @@
 require 'arrays'
    
-    describe "my_uniq" do
+describe "my_uniq" do
     let(:dups_array) {[4, 2, 3, 7, 2, 9, 9, 0, 7, 4, 1, 6]}
     #let(:dupless_array) {my_uniq(dups_array.dup)}
     let(:dupless_array)  {[4, 2, 3, 7, 9, 0, 1, 6]}
@@ -14,5 +14,14 @@ require 'arrays'
     it "doesn't change original" do
         expect(my_uniq(dups_array)).to_not change{dups_array}
     end
+
+    it "returns items in order they appeared" do
+        dups_hash = {}
+        dupless_hash = {}
+        dups_array.each {|num| dups_hash[num] = 1}
+        my_uniq(dups_array).each {|num| dupless_hash[num] = 1}
+        expect(dups_hash.keys).to eq(dupless_hash.keys)
+    end
+
 end
 
